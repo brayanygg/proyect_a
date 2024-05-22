677ae0699c3187995d197100fcb39b62fbaafd11
brayan <brayangafaro475@gmail.com>
2024-05-22 19:33:57 -0400

    agregada resolucion y posicion inicial

---
+++ app.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+
+root.title('Autenticacion')
+
+root.geometry('925x500+300+200')
+
+root.configure(bg="#fff")
+
+root.resizable(False, False)
+
+root.mainloop()
