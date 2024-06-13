216b15610c31f9e54660712bf25c105e891d168f
brayan <brayangafaro475@gmail.com>
2024-06-13 15:05:13 -0400

    agg 1st version de acceso con rangos

--- app.py
+++ app.py
@@ -3,29 +3,38 @@ from tkinter import messagebox
 
 root = Tk()
 
-root.title('Autenticacion')
+root.title('Login')
 root.geometry('925x500+300+200')
 root.configure(bg="#fff")
 root.resizable(False, False)
 
 img = PhotoImage(file='./assets/login.png')
-Label(root, image=img, bg='white').place(x=50, y=50)
+Label(root, image=img, bg='white').place(x=120, y=80)
 
 frameAutenticador = Frame(root, width=350, height=350, bg='white')
 
 frameAutenticador.place(x=480, y=70)
 
-cabecera = Label(frameAutenticador, text='Iniciar seción', fg='#57a1f8', bg='white', font=('TKDefaultFont', 23, 'bold'))
+cabecera = Label(frameAutenticador, text='Iniciar sesión', fg='#57a1f8', bg='white', font=('TKDefaultFont', 23, 'bold'))
 cabecera.place(x=55, y=5)
 
-def borrar(e):
+def autenticar():
+    nombre=usuario.get()
+    password=contrasena.get()
+
+    if nombre == 'admin' and password == 'admin':
+        print('acceso consedido rango admin')
+    else:
+        print('Error')
+
+def dentro(e):
     nombre = usuario.get()
     if nombre == "Nombre de usuario":
         usuario.delete(0, 'end')
         usuario.configure(fg="black")
         lineaUsuario.configure(bg="black")
 
-def insertar(e):
+def fuera(e):
     nombre = usuario.get()
     if nombre == "":
         usuario.insert(0,'Nombre de usuario')
@@ -35,22 +44,22 @@ def insertar(e):
 usuario = Entry(frameAutenticador, width=31, fg='gray', highlightthickness=0, relief=FLAT, font=('TKDefaultFont', 11))
 usuario.place(x=30, y=80)
 usuario.insert(0,'Nombre de usuario')
-usuario.bind('<FocusIn>', borrar)
-usuario.bind('<FocusOut>', insertar)
+usuario.bind('<FocusIn>', dentro)
+usuario.bind('<FocusOut>', fuera)
 
 # linea debajo del usernameInput
 lineaUsuario = Frame(frameAutenticador,width=295,height=2,bg='gray')
 lineaUsuario.place(x=25, y=107)
 
 
-def borrar(e):
+def dentro(e):
     contra = contrasena.get()
     if contra == "Contraseña":
         contrasena.delete(0, 'end')
         contrasena.configure(fg="black")
         lineaContra.config(bg="black")
 
-def insertar(e):
+def fuera(e):
     contra = contrasena.get()
     if contra == "":
         contrasena.insert(0,'Contraseña')
@@ -60,13 +69,13 @@ def insertar(e):
 contrasena = Entry(frameAutenticador, width=31, fg='gray', highlightthickness=0, relief=FLAT, font=('TKDefaultFont', 11))
 contrasena.place(x=30, y=150)
 contrasena.insert(0,'Contraseña')
-contrasena.bind('<FocusIn>', borrar)
-contrasena.bind('<FocusOut>', insertar)
+contrasena.bind('<FocusIn>', dentro)
+contrasena.bind('<FocusOut>', fuera)
 
 # linea debajo del contraseña input
 lineaContra = Frame(frameAutenticador,width=295,height=2,bg='gray')
 lineaContra.place(x=25, y=177)
 
-Button(frameAutenticador, width=29, pady=7, text='Verificar', bg='#57a1f8', cursor='hand2', fg='white', border=0).place(x=45, y=204)
+Button(frameAutenticador, width=29, pady=7, text='Verificar', bg='#57a1f8', cursor='hand2', fg='white', border=0, command=autenticar).place(x=45, y=204)
 
 root.mainloop()
