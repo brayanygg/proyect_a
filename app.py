83bedfccbb32ed242f0ce763e2e1c730e50c5bbe
brayan <brayangafaro475@gmail.com>
2024-06-26 17:23:40 -0400

    cambio de estilo e implementacion de ventana no ajustable

--- app.py
+++ app.py
@@ -9,24 +9,30 @@ class User():
         self.telefono = telefono
         self.direccion = direccion
         self.solvencia = solvencia
+k=0
+clientes = []
 
-user1=User('30853248','brayan', 'gafaro', '04141754875', 'romulo', True)
-user2=User('30853853','stiven', 'gonzales', '04241654275', 'hoyo', False)
-user3=User('30658975', 'samuel','bueno', '04248964785', 'tariba', True)
+while k < 15:
 
-clientes = [user1,user2,user3]
+    clientes.append(User('30853248','brayan', 'gafaro', '04141754875', 'romulo', True))
+
+    k = k+1
+# user2=User('30853853','stiven', 'gonzales', '04241654275', 'hoyo', False)
+# user3=User('30658975', 'samuel','bueno', '04248964785', 'tariba', True)
+
+# clientes = [user1,user2,user3]
 
 class AppMain(Top):
 
     def __init__(self, root,titulo, posYUbi, colorFondo):
         super().__init__(root,titulo, posYUbi, colorFondo)
 
-        self.ventana.resizable(1, 1)
+        self.ventana.resizable(0, 0)
         self.navBar()
         self.mainContent()
 
     def navBar(self):
-        self.navFrame = Frame(self.ventana,bg="black")
+        self.navFrame = Frame(self.ventana,bg="#171a4a")
         self.navFrame.pack(side=TOP,  fill=X)
         
         self.navFrame.columnconfigure(0, weight=9)  # gap column
@@ -42,33 +48,63 @@ class AppMain(Top):
         self.separador = Frame(self.ventana, bg='#fff', height=5)
         self.separador.pack(fill=X)
 
-        self.mainFrame = Frame(self.ventana, bg='white', padx=5)
-        self.mainFrame.pack(fill=X)
+        self.framePrincipal = Frame(self.ventana,bg="#fff")
+        self.framePrincipal.pack(side=TOP, fill=BOTH, expand=1)
 
-        self.mainFrame.columnconfigure(0, weight=2)  
-        self.mainFrame.columnconfigure(1, weight=2)  
-        self.mainFrame.columnconfigure(2, weight=2)  
-        self.mainFrame.columnconfigure(3, weight=2)
-        self.mainFrame.columnconfigure(4, weight=2)  
-        self.mainFrame.columnconfigure(5, weight=2)  
-        self.mainFrame.columnconfigure(6, weight=1)  
-        self.mainFrame.columnconfigure(7, weight=1)
+        self.myCanvas = Canvas(self.framePrincipal, bg='#fff')
+        self.myCanvas.pack(side=LEFT, fill=BOTH, expand=1)
 
-        Label(self.mainFrame, text=('Nombre'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=0,row=0,sticky="nsew")
-        Label(self.mainFrame, text=('Apellido'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=1,row=0, sticky="nsew")
-        Label(self.mainFrame, text=('Cedula'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=2,row=0, sticky="nsew")
-        Label(self.mainFrame, text=('Telefono'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=3,row=0,sticky="nsew")
-        Label(self.mainFrame, text=('Solvencia'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=4,row=0,sticky="nsew")
-        Label(self.mainFrame, text=('Direccion'),font=('TKDefaultFont', 14), bg='#24489C', fg='#fff').grid(column=5,row=0,sticky="nsew")
-        Frame(self.mainFrame, bg='#24489C').grid(column=6, row=0, columnspan=2, sticky=NSEW)
+        self.myScrollbar = Scrollbar(self.framePrincipal, orient=VERTICAL, command=self.myCanvas.yview)
+        self.myScrollbar.pack(side=RIGHT, fill=Y)
 
-        Frame(self.mainFrame,height=2,bg='black').grid(column=0, row=1, columnspan=8, sticky=EW)
+        self.myCanvas.configure(yscrollcommand=self.myScrollbar.set)
+        self.myCanvas.bind('<Configure>',lambda e: self.myCanvas.configure(scrollregion=self.myCanvas.bbox(ALL)))
 
-        Label(self.mainFrame, text=('brayan'),font=('TKDefaultFont', 12)).grid(column=0,row=2,sticky="nsew")
-        Label(self.mainFrame, text=('gafaro'),font=('TKDefaultFont', 12)).grid(column=1,row=2, sticky="nsew")
-        Label(self.mainFrame, text=('30853248'),font=('TKDefaultFont', 12)).grid(column=2,row=2, sticky="nsew")
-        Label(self.mainFrame, text=('04141754875'),font=('TKDefaultFont', 12)).grid(column=3,row=2,sticky="nsew")
-        Label(self.mainFrame, text=('Si'),font=('TKDefaultFont', 12)).grid(column=4,row=2,sticky="nsew")
-        Label(self.mainFrame, text=('romulo'),font=('TKDefaultFont', 12)).grid(column=5,row=2,sticky="nsew")
-        Button(self.mainFrame,text="Modificar",font=('TKDefaultFont', 10), bg='yellow', highlightthickness=0, relief=FLAT).grid(column=6,row=2)
-        Button(self.mainFrame,text="Eliminar",font=('TKDefaultFont', 10), bg='red', highlightthickness=0, relief=FLAT).grid(column=7,row=2)
+        self.frameContenedor = Frame(self.myCanvas)
+
+        self.myCanvas.create_window((0,0), window=self.frameContenedor, anchor=NW)
+
+        self.frameContenedor.columnconfigure(0, weight=2)  
+        self.frameContenedor.columnconfigure(1, weight=2)  
+        self.frameContenedor.columnconfigure(2, weight=2)  
+        self.frameContenedor.columnconfigure(3, weight=2)
+        self.frameContenedor.columnconfigure(4, weight=2)  
+        self.frameContenedor.columnconfigure(5, weight=2)  
+        self.frameContenedor.columnconfigure(6, weight=1)  
+        self.frameContenedor.columnconfigure(7, weight=1)
+
+        self.nombreC = Label(self.frameContenedor, text=('Nombre'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=10)
+        self.nombreC.grid(column=0,row=0,sticky="nsew")
+
+        self.apellidoC = Label(self.frameContenedor, text=('Apellido'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=10)
+        self.apellidoC.grid(column=1,row=0, sticky="nsew")
+
+        self.cedulaC = Label(self.frameContenedor, text=('Cedula'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=10)
+        self.cedulaC.grid(column=2,row=0, sticky="nsew")
+        
+        self.telefonoC = Label(self.frameContenedor, text=('Telefono'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=12)
+        self.telefonoC.grid(column=3,row=0,sticky="nsew")
+
+        self.solvenciaC = Label(self.frameContenedor, text=('Solvencia'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=8)
+        self.solvenciaC.grid(column=4,row=0,sticky="nsew")
+
+        self.direccionC = Label(self.frameContenedor, text=('Direccion'),font=('TKDefaultFont', 14), bg='#57a1f8', fg='#fff', width=10)
+        self.direccionC.grid(column=5,row=0,sticky="nsew")
+
+        self.botonesDuoC = Frame(self.frameContenedor, bg='#57a1f8')
+        self.botonesDuoC.grid(column=6, row=0, columnspan=2, sticky=NSEW)
+
+        limite = len(clientes)
+        i = 0
+        while(i < limite ):
+            
+            Label(self.frameContenedor, text=(clientes[i].nombre),font=('TKDefaultFont', 12), bg="#fff").grid(column=0,row=i+1,sticky=NSEW, )
+            Label(self.frameContenedor, text=(clientes[i].apellido),font=('TKDefaultFont', 12), bg="#fff").grid(column=1,row=i+1, sticky=NSEW, )
+            Label(self.frameContenedor, text=(clientes[i].cedula),font=('TKDefaultFont', 12), bg="#fff").grid(column=2,row=i+1, sticky=NSEW, )
+            Label(self.frameContenedor, text=(clientes[i].telefono),font=('TKDefaultFont', 12), bg="#fff").grid(column=3,row=i+1,sticky=NSEW, )
+            Label(self.frameContenedor, text=('Si'),font=('TKDefaultFont', 12), bg="#fff").grid(column=4,row=i+1,sticky=NSEW, )
+            Label(self.frameContenedor, text=(clientes[i].direccion),font=('TKDefaultFont', 12), bg="#fff").grid(column=5,row=i+1,sticky=NSEW, )
+            Button(self.frameContenedor,text='Modificar',font=('TKDefaultFont', 10), bg='#2f2c79', fg="#fff", highlightthickness=0, relief=FLAT, cursor="hand2").grid(column=6,row=i+1, pady=(4,0))
+            Button(self.frameContenedor,text='Eliminar',font=('TKDefaultFont', 10), bg='#2f2c79',fg="#fff", highlightthickness=0, relief=FLAT, cursor="hand2").grid(column=7,row=i+1, pady=(2,0), padx=(4,0))
+            
+            i = i+1
